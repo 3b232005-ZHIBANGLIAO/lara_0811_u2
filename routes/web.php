75ced77cb939b71ff4f223eb25ba12993d8b1f54
3b232005 <3b232005@gm.student.ncut.edu.tw>
2025-11-01 01:56:06 +0800

    設定 Route 反應

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',  function()  { 
+     return  view('welcome'); 
+});
+
+Route::get('/',  function()  { 
+     return  'welcome'; 
+}); 
+
+Route::get('r1',  function()  { 
+     return  redirect('r2'); 
+});
+Route::get('r2',  function()  { 
+     return  view('welcome'); 
 });
