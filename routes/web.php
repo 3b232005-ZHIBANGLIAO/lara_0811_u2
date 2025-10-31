fd5103b37bc56314430181099803dfab193e7aba
3b232005 <3b232005@gm.student.ncut.edu.tw>
2025-11-01 02:05:26 +0800

    設定 Route 接收參數

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',  function()  { 
-     return  view('welcome'); 
+Route::get('/', function() {
+    return view('welcome');
 });
 
-Route::get('/',  function()  { 
-     return  'welcome'; 
-}); 
-
-Route::get('r1',  function()  { 
-     return  redirect('r2'); 
+Route::get('r1', function() {
+    return redirect('r2');
 });
-Route::get('r2',  function()  { 
-     return  view('welcome'); 
+
+Route::get('r2', function() {
+    return view('welcome');
+});
+
+Route::get('hello/{name?}', function($name = 'Everybody') {
+    return 'Hello, ' . $name;
 });
